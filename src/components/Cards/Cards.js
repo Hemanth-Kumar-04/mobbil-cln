dac02415c4bf49ad0ca42b38b311cee8fcf07ed7
Hemanth-Kumar-04 <hemanthkumar.k.r04@gmail.com>
2023-11-12 16:30:52 +0530

    cards

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -4,16 +4,18 @@ import "./Cards.css";
 const Cards = () => {
   return (
     <div className="card">
+      <div className="img-in">
       <img
         src="https://w7.pngwing.com/pngs/166/320/png-transparent-rectangle-shape-rectangle-purple-blue-angle.png" // Replace with your image URL
         alt="Card Image"
         className="card-img"
       />
+      </div>
       <div className="card-content">
-        <div className="header">
+        < div className="header">
+          <img src="https://w7.pngwing.com/pngs/166/320/png-transparent-rectangle-shape-rectangle-purple-blue-angle.png" className="rounded-logo" />
+          
           <h2 className="card-title">Card Title</h2>
-          {/* Add your rounded logo or icon here */}
-          <div className="rounded-logo"></div>
         </div>
       </div>
     </div>
--- src/screens/Browse.js
+++ src/screens/Browse.js
@@ -6,7 +6,12 @@ const Browse = () => {
     return (
         <div className="browse">
         <h1>Browse</h1>
+        <div className="browse-cards">
         <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        </div>
         </div>
         )
     }
